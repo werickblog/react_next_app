6036d4a9f87775dc1cd02a978dc7814564e34681
tesh254 <ewachira254@gmail.com>
2019-07-09 17:20:31 +0300

    Initial commit

---
+++ components/nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link' // Next module that helps with routing
+
+const Nav = () => (
+  <nav>
+    
+  </nav>
+)
+
+export default Nav
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react' // We are writing react so we probably need this
+import Link from 'next/link' // the routing library for next
+import Head from '../components/head' // component that updates metadata for each page rendered
+import Nav from '../components/nav' // reusable navbar component
+
+const Home = () => (
+  <div>
+      Next meets ReactJS
+  </div>
+)
+
+export default Home
